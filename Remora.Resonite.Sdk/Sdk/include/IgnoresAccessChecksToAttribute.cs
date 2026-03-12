852ff75806110c3196e5bcb0ff973ac4a87fb8d9
Arne Kiesewetter <banane9@live.de>
2026-03-12 19:06:37 +0100

    Add Test projects for RML and Bepis; fix missing IgnoresAccessChecksToAttribute for RML

---
+++ Remora.Resonite.Sdk/Sdk/include/IgnoresAccessChecksToAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    internal sealed class IgnoresAccessChecksToAttribute : Attribute
+    {
+        public IgnoresAccessChecksToAttribute(string assemblyName)
+        {
+            AssemblyName = assemblyName;
+        }
+
+        public string AssemblyName { get; }
+    }
+}
